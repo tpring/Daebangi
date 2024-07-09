069f266c84f9c6e67f303ac801456e3139d2cba5
신자영 <144031936+tpring@users.noreply.github.com>
2024-07-09 21:27:40 +0900

    Update page.tsx
    Edit

--- src/app/(root)/detail/[id]/page.tsx
+++ src/app/(root)/detail/[id]/page.tsx
@@ -1,10 +1,11 @@
 import Comment from "@/components/comment/Comment";
+import StoreInformation from "@/components/detailMap/StoreInformation";
 
 const DetailPage = () => {
   return (
-    <div>
-      Detail
+    <div className="reactive-body mx-auto">
       <div>
+        <StoreInformation />
         <Comment />
       </div>
     </div>
