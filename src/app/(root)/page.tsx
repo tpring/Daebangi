bca1f9d27e14bfcd7c89248b144ae0a09212da35
heesu21 <gmltncpfl1@naver.com>
2024-07-09 17:14:09 +0900

    feature: bakerycard ui

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,29 +1,9 @@
-import Image from "next/image";
+import BakeryCard from "../../components/commons/BakeryCard";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <div className="max-w-xs border border-gray-300 rounded-lg p-4 shadow-md text-center">
-          {/* <Image
-            src=""
-            alt=""
-            width={200}
-            height={200}
-          /> */}
-          <div className="text-lg font-bold text-black mt-2">
-            성심당 대전역점{" "}
-            <span className="felx left text-red-500 text-xl">❤️</span>
-          </div>
-          <div className="text-gray-700 text-md mt-2">
-            <span className="text-gray-700 text-lg align-middle mr-1">📞</span>{" "}
-            1588-8069
-          </div>
-          <div className="text-gray-700 text-sm mt-1">
-            대전 동구 중앙로 215 대전역사 2F 1대전역 4번 출구에서 38m
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <BakeryCard />
+    </div>
   );
 }
