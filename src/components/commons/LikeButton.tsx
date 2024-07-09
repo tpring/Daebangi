15aa7515028fa43d797e201ddba35aedce76e664
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-09 17:59:25 +0900

    fix : inappropriate component file type

--- src/components/commons/LikeButton.tsx
+++ src/components/commons/LikeButton.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 
-const LikeButton = () => {
+const LikeButton: React.FC = () => {
   const [isLiked, setIsLiked] = useState(false);
 
   const handleToggleLike = () => {
