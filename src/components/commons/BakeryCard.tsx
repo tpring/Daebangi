624e030ce0f5643f464f0b659b8eae6da771b154
heesu21 <gmltncpfl1@naver.com>
2024-07-10 22:47:01 +0900

    feature: default이미지,연락처추가

--- src/components/commons/BakeryCard.tsx
+++ src/components/commons/BakeryCard.tsx
@@ -10,22 +10,23 @@ type BakeryCard = {
 import Image from "next/image";
 import phoneIcon from "../../../public/image/icons/phone.png";
 import LikeButton from "./LikeButton";
-import { useEffect, useState } from "react";
-import { createClient } from "@/supabase/client";
+import defaultImg from "../../../public/image/noimg.jpg";
 
 export const BakeryCard = ({ image, name, phone, address }: BakeryCard) => {
   return (
     <>
       <div className="flex justify-center items-center">
-        <div className="bg-base max-w-xs border border-point rounded-lg p-4 shadow-md text-center">
-          <div className="flex justify-center items-center">
-            <Image
-              className="p-2 "
-              src={image}
-              alt="bread img"
-              width={500}
-              height={500}
-            />
+        <div className="bg-base min-w-96 max-w-xs border border-point rounded-lg p-4 shadow-md text-center overflow-hidden min-h-80 h-[509px]">
+          <div className="flex justify-center items-center min-h-80">
+            <div className="flex items-center aspect-square">
+              <Image
+                className="p-2 w-full "
+                src={image || defaultImg}
+                alt="bread img"
+                width={500}
+                height={500}
+              />
+            </div>
           </div>
           <div className="flex justify-between items-center text-lg font-bold text-black mt-2">
             {name}
@@ -45,9 +46,9 @@ export const BakeryCard = ({ image, name, phone, address }: BakeryCard) => {
                 />
               </span>{" "}
             </span>
-            {phone}
+            {phone ? phone : "매장번호 미제공"}
           </div>
-          <div className="text-gray-700 text-sm mt-1 p-1 text-left">
+          <div className="text-gray-700 text-sm mt-1 p-1 text-left truncate">
             {address}
           </div>
         </div>
