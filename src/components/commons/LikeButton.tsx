83fcdb4d80f3bb0ed94cccb63d8ceaa55535f9bd
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-11 21:58:48 +0900

    fix : prevent page navigation on like button click

--- src/components/commons/LikeButton.tsx
+++ src/components/commons/LikeButton.tsx
@@ -2,28 +2,29 @@
 
 import { checkLikeStatus, toggleLikeStatus } from "@/app/api/(supabase)/(like)/route";
 import { useUserStore } from "@/store/userStore";
-
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-interface LikeButtonProp {
+type LikeButtonProp = {
   bakeryId: string;
-}
+};
+
+type UserStoreState = {
+  userId: string | null;
+};
 
 const LikeButton: React.FC<LikeButtonProp> = ({ bakeryId }) => {
   const [isLiked, setIsLiked] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  const { userId } = useUserStore((state) => ({
+  const { userId } = useUserStore((state: UserStoreState) => ({
     userId: state.userId,
   }));
 
   const fetchLikeStatus = async () => {
-
     if (!userId) return;
     try {
       const status = await checkLikeStatus(userId as string, bakeryId);
-
       setIsLiked(status);
     } catch (error) {
       console.error(error);
@@ -36,12 +37,8 @@ const LikeButton: React.FC<LikeButtonProp> = ({ bakeryId }) => {
     fetchLikeStatus();
   }, [userId]);
 
-
-  const handleToggleLike = async () => {
-    if (userId === null) {
-      alert("로그인 해주세요.");
-      return false;
-    }
+  const handleToggleLike = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
 
     try {
       const result = await toggleLikeStatus(isLiked, userId, bakeryId);
@@ -60,20 +57,9 @@ const LikeButton: React.FC<LikeButtonProp> = ({ bakeryId }) => {
   return (
     <button onClick={handleToggleLike}>
       {isLiked ? (
-        <Image
-          src="/image/icons/heartActive.png"
-          width={20}
-          height={20}
-          alt="liked image"
-        />
+        <Image src="/image/icons/heartActive.png" width={20} height={20} alt="liked image" />
       ) : (
-        <Image
-          src="/image/icons/heart.png"
-          width={20}
-          height={20}
-          alt="unliked image"
-        />
-
+        <Image src="/image/icons/heart.png" width={20} height={20} alt="unliked image" />
       )}
     </button>
   );
