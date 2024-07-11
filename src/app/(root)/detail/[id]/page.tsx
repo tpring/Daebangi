b8375d44a2f4f26871b713e11ba74352bf3d0316
tpring <pororo1@naver.com>
2024-07-11 16:50:35 +0900

    refactor: auth refactor #64

--- src/app/(root)/detail/[id]/page.tsx
+++ src/app/(root)/detail/[id]/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import Comment from '@/components/comment/Comment';
+import CommentList from '@/components/comment/CommentList';
 import StoreInformation from '@/components/detailMap/StoreInformation';
-
 const SkeletonLoader = () => (
     <div className="w-full h-96 md:h-500 lg:h-600">
         <div className="w-full h-full bg-gray-100 animate-pulse">
@@ -10,32 +9,26 @@ const SkeletonLoader = () => (
         </div>
     </div>
 );
-
 const KakaoMap = dynamic(() => import('@/components/detailMap/KakaoMap'), {
     ssr: false,
     loading: () => <SkeletonLoader />,
 });
-
 type SearchParams = {
     name?: string;
     address?: string;
     bakeryId?: string;
 };
-
 type DetailPageProps = {
     searchParams: SearchParams;
 };
-
 const DetailPage: React.FC<DetailPageProps> = ({ searchParams }) => {
     const { name, address, bakeryId } = searchParams;
-
     return (
         <div className="reactive-body mx-auto">
             {name && address ? <KakaoMap name={name} address={address} /> : null}
             {bakeryId ? <StoreInformation bakeryId={bakeryId} /> : null}
-            <Comment />
+            {bakeryId ? <CommentList bakery_id={bakeryId} /> : null}
         </div>
     );
 };
-
 export default DetailPage;
--- src/app/(root)/login/page.tsx
+++ src/app/(root)/login/page.tsx
@@ -1,105 +1,101 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import LogoBread from '../../../../public/image/breads/LogoBread.png';
-import { createClient } from '../../../supabase/client';
-import { useRouter } from 'next/navigation';
-import { useUserStore } from '@/store/userStore';
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import LogoBread from "../../../../public/image/breads/LogoBread.png";
+import { useRouter } from "next/navigation";
+import { useUserStore } from "@/store/userStore";
+import { login, getUserData } from "../../api/(supabase)/auth/route";
 
 const LoginPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const supabase = createClient();
-    const setUser = useUserStore((state) => state.setUser);
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const setUser = useUserStore((state) => state.setUser);
+  const router = useRouter();
 
-    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        // 유효성 검사
-        if (!email || !password) {
-            alert('이메일과 비밀번호를 모두 입력해주세요.');
-            return;
-        }
+    // 유효성 검사
+    if (!email || !password) {
+      alert("이메일과 비밀번호를 모두 입력해주세요.");
+      return;
+    }
 
-        if (password.length < 6) {
-            alert('비밀번호는 6자리 이상이어야 합니다.');
-            return;
-        }
+    if (password.length < 6) {
+      alert("비밀번호는 6자리 이상이어야 합니다.");
+      return;
+    }
 
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
-        });
+    try {
+      const { data, error } = await login(email, password);
 
-        if (error) {
-            // Supabase의 오류 메시지에 따라 유효성 검사 처리
-            if (error.message.includes('Invalid login credentials')) {
-                alert('이메일이나 비밀번호가 잘못되었습니다.');
-            } else {
-                alert('로그인 실패: ' + error.message);
-            }
-            console.error('error:', error);
+      if (error) {
+        // Supabase의 오류 메시지에 따라 유효성 검사 처리
+        if (error.message.includes("Invalid login credentials")) {
+          alert("이메일이나 비밀번호가 잘못되었습니다.");
         } else {
-            if (data.user) {
-                // Supabase에서 사용자 정보 가져오기
-                const { data: userData, error: userError } = await supabase
-                    .from('user')
-                    .select('user_id, email, nickname, profile')
-                    .eq('email', email)
-                    .single();
-
-                if (userError) {
-                    alert('사용자 정보를 가져오는 데 실패했습니다.');
-                    console.error('userError:', userError);
-                } else {
-                    // Zustand 상태 업데이트
-                    setUser(userData.user_id, userData.email, userData.nickname ?? '', userData.profile ?? '');
-                    alert('로그인 성공');
-                    router.push('/');
-                }
-            }
+          alert("로그인 실패: " + error.message);
         }
-        return data;
-    };
+        console.error("error:", error);
+      } else {
+        if (data.user) {
+          // Supabase에서 사용자 정보 가져오기
+          const { data: userData, error: userError } = await getUserData(email);
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <Image src={LogoBread} alt="Logo Bread" width={288} height={201} priority className="mb-[-20px]" />
-            <label className="font-secondary text-[60px]">대빵이</label>
-            <form onSubmit={handleLogin}>
-                <p className="shared-text">이메일</p>
-                <input
-                    className="shared-input mb-4 focus:outline-[#C9AB9C]"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <p className="shared-text">비밀번호</p>
-                <input
-                    className="shared-input mb-4 focus:outline-[#C9AB9C]"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <div>
-                    <button type="submit" className="shared-butten my-10 hover:bg-[#C9AB9C]">
-                        로그인
-                    </button>
-                </div>
-            </form>
-            <Link
-                href={'/signup'}
-                className="text-basics hover:text-teal-600 hover:bg-gray-200 hover:underline transition-colors duration-300 p-2 rounded-md"
-            >
-                회원가입 하러 가기
-            </Link>
+          if (userError) {
+            alert("사용자 정보를 가져오는 데 실패했습니다.");
+            console.error("userError:", userError);
+          } else {
+            // Zustand 상태 업데이트
+            setUser(userData.user_id, userData.email, userData.nickname ?? "", userData.profile ?? "");
+            alert("로그인 성공");
+            router.push("/");
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      alert("오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image src={LogoBread} alt="Logo Bread" width={288} height={201} priority className="mb-[-20px]" />
+      <label className="font-secondary text-[60px]">대빵이</label>
+      <form onSubmit={handleLogin}>
+        <p className="shared-text">이메일</p>
+        <input
+          className="shared-input mb-4 focus:outline-[#C9AB9C]"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p className="shared-text">비밀번호</p>
+        <input
+          className="shared-input mb-4 focus:outline-[#C9AB9C]"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div>
+          <button type="submit" className="shared-butten my-10 hover:bg-[#C9AB9C]">
+            로그인
+          </button>
         </div>
-    );
+      </form>
+      <Link
+        href={"/signup"}
+        className="text-basics hover:text-teal-600 hover:bg-gray-200 hover:underline transition-colors duration-300 p-2 rounded-md"
+      >
+        회원가입 하러 가기
+      </Link>
+    </div>
+  );
 };
 
 export default LoginPage;
