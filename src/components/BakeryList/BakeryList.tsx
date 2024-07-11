1be4d90b015a0e7b5d86d357cdfa4eb4286c66de
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-11 16:31:00 +0900

    feature : search bakery

--- src/components/BakeryList/BakeryList.tsx
+++ src/components/BakeryList/BakeryList.tsx
@@ -1,58 +1,66 @@
-'use client';
+"use client";
 
 type Bakery = {
-    bakery_id: string;
-    name: string;
-    image: string;
-    phone: string;
-    address: string;
+  bakery_id: string;
+  name: string;
+  image: string;
+  phone: string;
+  address: string;
 };
 
-import { useEffect, useState } from 'react';
-import { createClient } from '@/supabase/client';
-import { BakeryCard } from '../commons/BakeryCard';
-import Link from 'next/link';
+import { useEffect, useState } from "react";
+import { createClient } from "@/supabase/client";
+import { BakeryCard } from "../commons/BakeryCard";
+import Link from "next/link";
 
-export const BakeryList = () => {
-    const [breads, setBreads] = useState<Bakery[]>([]);
+interface BakeryListProp {
+  searchedBakeries?: Bakery[];
+}
 
-    useEffect(() => {
-        const supabase = createClient();
-        const fetchBreads = async () => {
-            const { data } = await supabase.from('bakery').select('*');
-            setBreads((data as Bakery[]) || []);
-        };
-        fetchBreads();
-    }, []);
+export const BakeryList = ({ searchedBakeries }: BakeryListProp) => {
+  const [breads, setBreads] = useState<Bakery[]>([]);
 
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {breads.map((bakery) => (
-                <Link
-                    href={{
-                        pathname: `/detail/${bakery.bakery_id}`,
-                        query: {
-                            name: bakery.name,
-                            address: bakery.address,
-                            image: bakery.image,
-                            phone: bakery.phone,
-                            bakeryId: bakery.bakery_id,
-                        },
-                    }}
-                    key={bakery.bakery_id}
-                    passHref
-                >
-                    <div>
-                        <BakeryCard
-                            bakeryId={bakery.bakery_id}
-                            image={bakery.image}
-                            name={bakery.name}
-                            phone={bakery.phone}
-                            address={bakery.address}
-                        />
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
+  useEffect(() => {
+    if (searchedBakeries) {
+      setBreads(searchedBakeries);
+    } else {
+      const supabase = createClient();
+      const fetchBreads = async () => {
+        const { data } = await supabase.from("bakery").select("*");
+        setBreads((data as Bakery[]) || []);
+      };
+      fetchBreads();
+    }
+  }, [searchedBakeries]);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {breads.map((bakery) => (
+        <Link
+          href={{
+            pathname: `/detail/${bakery.bakery_id}`,
+            query: {
+              name: bakery.name,
+              address: bakery.address,
+              image: bakery.image,
+              phone: bakery.phone,
+              bakeryId: bakery.bakery_id,
+            },
+          }}
+          key={bakery.bakery_id}
+          passHref
+        >
+          <div>
+            <BakeryCard
+              bakeryId={bakery.bakery_id}
+              image={bakery.image}
+              name={bakery.name}
+              phone={bakery.phone}
+              address={bakery.address}
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
