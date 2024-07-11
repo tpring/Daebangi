63c222cc964d34ab11754e42ed7ae781dc7b793d
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-11 12:32:08 +0900

    feature : set up supabase query for bakery search function

---
+++ src/app/api/(supabase)/(bakery)/route.ts
@@ -0,0 +1,22 @@
+import { createClient } from "@/supabase/client";
+
+const supabase = createClient();
+
+export const searchBakery = async (keyword: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("bakery")
+      .select("*")
+      .ilike("name", `%${keyword}%`);
+
+    if (error) {
+      console.error(error);
+      return false;
+    } else {
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
