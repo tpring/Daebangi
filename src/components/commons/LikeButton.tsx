571943e9e3231ad05643839272b2338c66476238
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-11 10:04:27 +0900

    chore : remove unused import

--- src/components/commons/LikeButton.tsx
+++ src/components/commons/LikeButton.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { createClient } from "@/supabase/client";
+"use client";
+
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import {
@@ -9,61 +9,71 @@ import {
 import { useUserStore } from "@/store/userStore";
 
 interface LikeButtonProp {
-    bakeryId: string;
+  bakeryId: string;
 }
 
 const LikeButton: React.FC<LikeButtonProp> = ({ bakeryId }) => {
-    const [isLiked, setIsLiked] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLiked, setIsLiked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const { userId } = useUserStore((state) => ({
-        userId: state.userId,
-    }));
+  const { userId } = useUserStore((state) => ({
+    userId: state.userId,
+  }));
 
-    const fetchLikeStatus = async () => {
-        try {
-            const status = await checkLikeStatus(userId, bakeryId);
-            setIsLiked(status);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const fetchLikeStatus = async () => {
+    try {
+      const status = await checkLikeStatus(userId, bakeryId);
+      setIsLiked(status);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        fetchLikeStatus();
-    }, []);
+  useEffect(() => {
+    fetchLikeStatus();
+  }, []);
 
-    const handleToggleLike = async () => {
-        if (userId === null) {
-            alert('로그인 해주세요.');
-            return false;
-        }
-
-        try {
-            const result = await toggleLikeStatus(isLiked, userId, bakeryId);
-            if (result) {
-                setIsLiked(!isLiked);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    if (isLoading) {
-        return null;
+  const handleToggleLike = async () => {
+    if (userId === null) {
+      alert("로그인 해주세요.");
+      return false;
     }
 
-    return (
-        <button onClick={handleToggleLike}>
-            {isLiked ? (
-                <Image src="/image/icons/heartActive.png" width={20} height={20} alt="liked image" />
-            ) : (
-                <Image src="/image/icons/heart.png" width={20} height={20} alt="unliked image" />
-            )}
-        </button>
-    );
+    try {
+      const result = await toggleLikeStatus(isLiked, userId, bakeryId);
+      if (result) {
+        setIsLiked(!isLiked);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <button onClick={handleToggleLike}>
+      {isLiked ? (
+        <Image
+          src="/image/icons/heartActive.png"
+          width={20}
+          height={20}
+          alt="liked image"
+        />
+      ) : (
+        <Image
+          src="/image/icons/heart.png"
+          width={20}
+          height={20}
+          alt="unliked image"
+        />
+      )}
+    </button>
+  );
 };
 
 export default LikeButton;
