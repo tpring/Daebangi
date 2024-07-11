0ec83e629febd3160a7466d965c8c2fa3a299719
Elephant24456 <rlaxortn0984@naver.com>
2024-07-12 00:15:43 +0900

    Fix: modal function modify #65

--- src/app/(root)/@modal/(.)modifyprofile/page.tsx
+++ src/app/(root)/@modal/(.)modifyprofile/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { useUserStore } from "@/store/userStore";
 import Image from 'next/image';
-import { useState } from 'react';
+import { HTMLInputAutoCompleteAttribute, useRef, useState } from 'react';
 import { createClient } from '../../../../supabase/client';
+import { useRouter } from 'next/navigation';
+
+
 
 //modifyprofile의 페이지를 인터셉트 하는 페이지 입니다.
 // 모달창
 const Page = () => {
+    const router = useRouter();
     const supabase = createClient()
     const { userId, email, nickname, profile } = useUserStore((state) => ({
         userId: state.userId as string,
@@ -18,6 +22,7 @@ const Page = () => {
     const [newNickname, setNewNickname] = useState(nickname);
     const [newProfile, setNewProfile] = useState(profile);
     const [description, setDescription] = useState('')
+    const inputref = useRef<HTMLInputElement>(null)
 
     const handleProfileChange = (e: any) => {
         setNewProfile(e.target.files[0]);
@@ -46,36 +51,50 @@ const Page = () => {
                 console.error('Error updating user data:', error);
             } else {
                 console.log('User data updated successfully:', data);
+                // 프로필 수정 완료 후 /mypage로 이동
+                router.back();
             }
         } catch (error) {
             console.error('Error updating user data:', error);
         }
     };
 
+
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white rounded-lg shadow-lg p-3 w-70 max-w">
-                <h2 className="text-2xl font-bold mb-4 text-center">프로필 수정</h2>
+                <h2 className="text-2xl font-bold mb-4 text-center shared-text">프로필 수정</h2>
                 <div className="flex justify-center">
                     <div className="w-32 h-32 bg-gray-300 rounded-full flex items-center justify-center cursor-pointer" >
                         {newProfile ? (
-                            <Image src={newProfile} alt="Profile" width={100} height={100} className="w-full h-full rounded-full" />
+                            <Image
+                                src={newProfile}
+                                alt="Profile"
+                                width={100}
+                                height={100}
+                                className="w-full h-full rounded-full"
+                                onClick={() => {
+                                    inputref.current?.click();
+                                }}
+                            />
                         ) : (
-                            <Image src={profile || ''} alt="Default Profile" className="w-full h-full rounded-full" />
+                            <Image src={profile || ''} alt="Default Profile" className="w-full h-full rounded-full" onClick={() => {
+                                inputref.current?.click()
+                            }} />
                         )}
                     </div>
-                    <input type="file" onChange={handleProfileChange} style={{ display: 'none' }} />
+                    <input type="file" onChange={handleProfileChange} ref={inputref} className="hidden" />
                 </div> <div className="mt-4 flex flex-col">
-                    <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center shared-text">
                         <p>닉네임</p>
                         <input className="shared-input" type="text" value={newNickname || ''} onChange={handleNicknameChange} />
                     </div>
-                    <div className="flex justify-between items-center mt-4">
+                    <div className="flex justify-between items-center mt-4 shared-text">
                         <p>소개</p>
                         <input className="shared-input" type="text" onChange={handleDescriptionChange} />
                     </div>
                     <div className="flex justify-end mt-4">
-                        <button className="font-secondary" onClick={handleSubmit}>프로필 수정 완료</button>
+                        <button className="font-secondary shared-text" onClick={handleSubmit}>프로필 수정 완료</button>
                     </div>
                 </div>
             </div>
