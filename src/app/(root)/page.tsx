aefadffeabcfa65ebbd4a26403cf9434eaaa8298
김민곤 <57051139+MinKonKim@users.noreply.github.com>
2024-07-12 18:37:07 +0900

    Update page.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -18,11 +18,11 @@ export default function Home() {
       <BouncingBread />
       <div className="relative">
         <div className="flex justify-center items-center h-60">
-          {/* 이부분은 값을 찾아서 다른 페이지로 넘깁니다. */}
+    
           <SearchBar onSearch={handleSearch} />
         </div>
         <div className="flex justify-center items-center">
-          {/* 이부분은 props가 있지만 없었습니다. */}
+        
           <BakeryList />
         </div>
       </div>
