96b9d316558b8fb7de5f504f18b12c0408ba5f98
tpring <pororo1@naver.com>
2024-07-14 15:57:22 +0900

    refactor : attow function

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,9 +1,9 @@
 import { Database } from "@/types/supabase";
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
+export const createClient = () => {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
-}
+};
