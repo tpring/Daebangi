a0fe95bed9aedb011590c7092698674ff6ea6297
tpring <pororo1@naver.com>
2024-07-14 19:57:04 +0900

    test

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,9 +1,5 @@
-import { Database } from "@/types/supabase";
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () => {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 };
