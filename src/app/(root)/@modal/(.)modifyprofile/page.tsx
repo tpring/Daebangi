a8d2d6a8ee9f53f837de0efa426860cdfe475c99
Elephant24456 <rlaxortn0984@naver.com>
2024-07-14 19:16:01 +0900

    Fix: modalpage redirect #126

--- src/app/(root)/@modal/(.)modifyprofile/page.tsx
+++ src/app/(root)/@modal/(.)modifyprofile/page.tsx
@@ -102,10 +102,10 @@ const Page = () => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-12 w-70 max-w">
+      <div className="bg-white rounded-lg shadow-lg p-3 w-70 max-w">
         <h2 className="mb-4 text-center text-title font-title">프로필 수정</h2>
         <div className="flex justify-center">
-          <div className="w-32 h-32 border border-[#ccc] rounded-full overflow-hidden bg-[#fdfbfb] flex items-center justify-center cursor-pointer">
+          <div className="w-32 h-32 bg-gray-300 rounded-full flex items-center justify-center cursor-pointer">
             {newProfile ? (
               <Image
                 src={profileUrl || ""}
@@ -142,7 +142,7 @@ const Page = () => {
             <input className="shared-input" type="text" value={description} onChange={handleDescriptionChange} />
           </div>
           <div className="flex justify-end mt-4">
-            <button className="shared-butten" onClick={handleSubmit}>
+            <button className="font-secondary shared-butten" onClick={handleSubmit}>
               프로필 수정 완료
             </button>
           </div>
--- src/app/(root)/modifyprofile/page.tsx
+++ src/app/(root)/modifyprofile/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 // 병렬 라우팅을 위한 페이지입니다
 const Page = () => {
-    return <>this is modifyprofile page</>;
+    const router = useRouter();
+
+    useEffect(() => {
+        // 페이지 로드 시 자동으로 '/mypage'로 리다이렉트
+        router.replace('/mypage');
+    }, [router]);
+
+    return null;
 }
 
 export default Page;
