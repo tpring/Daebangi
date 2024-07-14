a0ac6f8c1f90e9594bf47eae7f44ae8feda4e542
Elephant24456 <rlaxortn0984@naver.com>
2024-07-14 19:25:20 +0900

    Fix: modifyprofile fix2 #126

--- src/app/(root)/modifyprofile/page.tsx
+++ src/app/(root)/modifyprofile/page.tsx
@@ -5,19 +5,14 @@ import { useRouter } from 'next/navigation';
 
 // 병렬 라우팅을 위한 페이지입니다
 const Page = () => {
-<<<<<<< HEAD
     const router = useRouter();
 
     useEffect(() => {
-        // 페이지 로드 시 자동으로 '/mypage'로 리다이렉트
+        // 새로고침 시 자동으로 '/mypage'로 리다이렉트
         router.replace('/mypage');
     }, [router]);
 
     return null;
 }
-=======
-  return <>this is modifyprofile page</>;
-};
->>>>>>> 73ea2177c5b47b6417c215430312c754127acb1e
 
 export default Page;
